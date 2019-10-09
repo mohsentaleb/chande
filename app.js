f646012746a72f82a3dbb748b2ca384b109d0c84
Mahdi Taghizadeh <i@mahdi.ws>
2019-10-09 09:04:00 +0100

    Added GBP to the list of currencies

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 #!/usr/bin/env node
+
 const got = require('got');
 const AsciiTable = require('ascii-table');
 
 (async () => {
     const userCurrency = process.argv[2] ? process.argv[2].toLocaleLowerCase() : null;
-    const pricesJSON = await got('http://call4.tgju.org/ajax.json', { json: true });
-    const { current:prices } = pricesJSON.body;
- 
+    const pricesJSON = await got('http://call4.tgju.org/ajax.json', {
+        json: true
+    });
+    const {
+        current: prices
+    } = pricesJSON.body;
+
     let currencyValues = {
         usd: [prices.price_dollar_rl.l, prices.price_dollar_rl.h, prices.price_dollar_rl.p],
         eur: [prices.price_eur.l, prices.price_eur.h, prices.price_eur.p],
         try: [prices.price_try.l, prices.price_try.h, prices.price_try.p],
         aed: [prices.price_aed.l, prices.price_aed.h, prices.price_aed.p],
+        gbp: [prices.price_gbp.l, prices.price_gbp.h, prices.price_gbp.p],
     };
 
     if (userCurrency) {
         const supportedCurrencies = Object.keys(currencyValues);
-        
-        if (supportedCurrencies.includes(userCurrency) ) {
-            currencyValues = { [userCurrency]: currencyValues[userCurrency]};
-         }
+
+        if (supportedCurrencies.includes(userCurrency)) {
+            currencyValues = {
+                [userCurrency]: currencyValues[userCurrency]
+            };
+        }
     }
 
     let tableJSON = {
@@ -28,7 +36,7 @@ const AsciiTable = require('ascii-table');
         rows: []
     };
 
-    Object.keys(currencyValues).map( (currency, index) => {
+    Object.keys(currencyValues).map((currency, index) => {
         let currencyId = currency.toUpperCase();
         tableJSON.rows.push([++index, currencyId, ...currencyValues[currency]]);
     });
